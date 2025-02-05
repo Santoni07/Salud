e312a7e38b34c97da8316fe97b8f741701881d14
AleSantoni <alesantoni31@gmail.com>
2025-02-05 20:58:01 -0300

    "Backup antes de modificaciones en Profile y autenticación"

--- FichaMedica/account/admin.py
+++ FichaMedica/account/admin.py
@@ -2,16 +2,25 @@ from django.contrib import admin
 from .models import Profile
 from django.contrib import admin
 from .models import Profile
+from django.contrib.auth.hashers import make_password
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'nombre', 'apellido', 'dni', 'fecha_nacimiento', 'email', 'rol')  # Campos a mostrar en la lista
-    search_fields = ('user__username', 'nombre', 'apellido', 'email')  # Campos por los que se puede buscar
-    list_filter = ('rol',)  # Filtros por el rol
-    list_editable = ('rol',)  # Permitir edición directa del rol en la lista
-    
+    list_display = ('user', 'nombre', 'apellido', 'dni', 'fecha_nacimiento', 'email', 'rol')
+    search_fields = ('user__username', 'nombre', 'apellido', 'email')
+    list_filter = ('rol',)
+    list_editable = ('rol',)
+
+    def save_model(self, request, obj, form, change):
+        if not obj.user.pk:  # Solo para nuevos usuarios
+            obj.user.set_password(obj.user.password)  # Cifra la contraseña si es nueva
+            obj.user.save()
+        elif 'password' in form.changed_data:
+            obj.user.password = make_password(obj.user.password)  # Cifra si se cambió la contraseña
+            obj.user.save()
+        super().save_model(request, obj, form, change)
+
     def get_queryset(self, request):
-        # Personalizar la consulta para incluir información relacionada
         qs = super().get_queryset(request)
-        return qs.select_related('user')  # Mejorar el rendimiento al acceder a datos del usuario
+        return qs.select_related('user')
 
 admin.site.register(Profile, ProfileAdmin)
